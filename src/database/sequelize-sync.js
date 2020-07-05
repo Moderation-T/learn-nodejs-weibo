45613907050b6d28117098614f45be1cdc0239b5
yizhiyu <yizhiyu_web@163.com>
2020-07-05 14:01:33 +0800

    建立 sequelize 同步

--- src/database/sequelize-sync.js
+++ src/database/sequelize-sync.js
@@ -2,3 +2,20 @@
  * @description sequelize 同步 MySQL
  * @author 一只鱼
  */
+
+const seq = require('./sequelize');
+
+// 测试连接
+seq
+  .authenticate()
+  .then(() => {
+    console.log('连接正常');
+  })
+  .catch(() => {
+    console.log('连接异常');
+  });
+
+seq.sync({ force: true }).then(() => {
+  console.log('同步完成 并清空原有数据防止干扰');
+  process.exit();
+});
