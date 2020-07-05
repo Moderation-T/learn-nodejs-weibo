10cd383587b86a694e49b649fc52f43447946452
yizhiyu <yizhiyu_web@163.com>
2020-07-05 14:39:13 +0800

    配置 eslint

--- src/database/sequelize.js
+++ src/database/sequelize.js
@@ -5,7 +5,7 @@
 
 const Sequelize = require('sequelize');
 const { MYSQL_CONF } = require('../conf/database');
-const { isProd,isTest } = require('../utils/env');
+const { isProd, isTest } = require('../utils/env');
 
 const { database, user, password, host } = MYSQL_CONF;
 
@@ -14,16 +14,16 @@ const conf = {
   dialect: 'mysql',
 };
 
-if(isTest) {
-  conf.logging = ()=>{} // 测试环境下不打印 SQL 语句
+if (isTest) {
+  conf.logging = () => {}; // 测试环境下不打印 SQL 语句
 }
 
-if(isProd) {
+if (isProd) {
   conf.pool = {
-    max:5, // 连接池中最大连接数量
-    min:0, // 最小连接数量
-    idle：10000 // 如果一个连接池 10s 没有被使用，则释放
-  }
+    max: 5, // 连接池中最大连接数量
+    min: 0, // 最小连接数量
+    idle: 10000, // 如果一个连接池 10s 没有被使用，则释放
+  };
 }
 
 const seq = new Sequelize(database, user, password, conf);
