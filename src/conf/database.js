22dc48e4bfa511ccd59c2033ca9bcee2f7041128
yizhiyu <yizhiyu_web@163.com>
2020-07-05 13:51:24 +0800

    配置 sequelize

--- src/conf/database.js
+++ src/conf/database.js
@@ -1,3 +1,34 @@
-const REDIS_CONF = {};
-const SEQ_CONf = {};
-const MYSQL_CONF = {};
+const { isProd } = require('../utils/env');
+
+let REDIS_CONF = {
+  port: 6379,
+  host: 'localhost',
+};
+
+let MYSQL_CONF = {
+  host: 'localhost',
+  port: 3306,
+  user: 'root',
+  password: 'root123456',
+  database: 'learn-weibo',
+};
+
+// 生产环境下的配置
+if (isProd) {
+  REDIS_CONF = {
+    port: 6379,
+    host: 'localhost',
+  };
+  MYSQL_CONF = {
+    host: 'localhost',
+    port: 3306,
+    user: 'root',
+    password: 'root123456',
+    database: 'learn-weibo',
+  };
+}
+
+module.exports = {
+  REDIS_CONF,
+  MYSQL_CONF,
+};
--- src/database/sequelize.js
+++ src/database/sequelize.js
@@ -1,10 +1,22 @@
 const Sequelize = require('sequelize');
+const { MYSQL_CONF } = require('../conf/database');
+const { isProd } = require('../utils/env');
+
+const { database, user, password, host } = MYSQL_CONF;
 
 const conf = {
-  host: 'localhost',
+  host,
   dialect: 'mysql',
 };
 
-const seq = new Sequelize('learn-weibo', 'root', 'root123456', conf);
+if(isProd) {
+  conf.pool = {
+    max:5, // 连接池中最大连接数量
+    min:0, // 最小连接数量
+    idle：10000 // 如果一个连接池 10s 没有被使用，则释放
+  }
+}
 
-module.exports = seq;  
+const seq = new Sequelize(database, user, password, conf);
+
+module.exports = seq;
