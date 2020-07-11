0d4bc3ae49d60f2ff0b502a97d3004e01020e5d4
yizhiyu <yizhiyu_web@163.com>
2020-07-11 15:55:04 +0800

    检测用户是否登陆

--- src/routes/view/users.js
+++ src/routes/view/users.js
@@ -5,8 +5,36 @@
 
 const router = require('koa-router')();
 
+/**
+ * 检测用户是否已登录
+ *
+ * @param {*} ctx koa ct x
+ * @returns 返回用户是否登陆信息
+ */
+function checkoutUserIsLogin(ctx) {
+  let data = {
+    isLogin: false, // 默认未登陆
+  };
+
+  if (ctx.session && ctx.session.userInfo) {
+    // 已登录
+    data = {
+      isLogin: true,
+      userName: ctx.session.userInfo.userName,
+    };
+  }
+
+  return data;
+}
+
 router.get('/login', async (ctx, next) => {
-  await ctx.render('login', {});
+  const { isLogin, userName } = checkoutUserIsLogin(ctx);
+  console.log(isLogin, userName);
+
+  await ctx.render('login', {
+    isLogin,
+    userName,
+  });
 });
 
 router.get('/register', async (ctx, next) => {
